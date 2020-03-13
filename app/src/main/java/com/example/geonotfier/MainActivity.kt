e4bb42eed11bddea6ddceb508345e00e64691e24
Jure <jure.mazej2000@gmail.com>
2020-03-13 10:49:46 +0100

    new activity added

--- app/src/main/java/com/example/geonotfier/MainActivity.kt
+++ app/src/main/java/com/example/geonotfier/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.geonotfier
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openNewLocation(view: View) {
+        val intent = Intent(this, NewLocation::class.java)
+
+        val extras = Bundle()
+        intent.putExtras(extras)
+
+        startActivity(intent)
     }
 }
